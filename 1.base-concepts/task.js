a144207e77bfa3b1d5b6001b212e7c0bace70d18
Olga Oblat <oa.oblat@gmail.com>
2023-11-13 19:48:37 +0300

    chenged task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
+// Задача 1. Программа для решения квадратных уравнений (ax² + bx + c = 0)
+
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b**2 - 4*a*c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push(-b/(2*a));
+    return arr;
+  } else {
+    arr.push((-b + Math.sqrt(d))/(2*a));
+    arr.push((-b - Math.sqrt(d))/(2*a));
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonthly = (percent / 100) / 12;
+  let principleAmount = amount - contribution;
+  let paymentMonthly = principleAmount * (percentMonthly+ (percentMonthly / (((1 + percentMonthly)**countMonths) - 1)));
+  let totalAmount = parseFloat((paymentMonthly * countMonths).toFixed(2));
+  return totalAmount;
 }
+
