658957553aa7f36f42cebd7d8d7c71df388d5618
Olga Oblat <oa.oblat@gmail.com>
2023-11-16 13:38:32 +0300

    changed task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
+// Задача №1 Сравнить массивы
+
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
+// Задача №2 Получение среднего возраста пользователей одного пола
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users
+        .filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, item, i, arr) => {
+            acc += item;
+            if (i === arr.length - 1){
+                return acc / arr.length;
+            }
+            return acc;
+        }, 0);
+    return result;
 }
