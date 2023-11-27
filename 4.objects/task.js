67d8204f6c591cb8b3f8c3d4d062bb0bfc6136ef
Olga Oblat <oa.oblat@gmail.com>
2023-11-27 17:37:32 +0300

    changed task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+};
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks) {
+		this.marks.push(...marksToAdd);
+	} else {
+		console.log('Студент отчислен!');
+	}
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	let sum = this.marks.reduce((total, mark) => total + mark, 0);
+	return sum / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
+
+let student1 = new Student('Ольга', 'женский', 22);
+let student2 = new Student('Иван', 'мужской', 21);
